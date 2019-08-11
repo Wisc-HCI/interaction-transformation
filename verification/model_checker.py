8d220d5dbbdc39ced93aa0e94376483a47bfc885
David Porfirio <dporfirio@wisc.edu>
2019-08-11 17:19:19 -0500

    adding debug

--- verification/model_checker.py
+++ verification/model_checker.py
@@ -114,5 +114,6 @@ class ModelChecker:
             outfile.write(file_string)
 
     def check(self):
+        print("checking model")
         results, counterexamples = tools.tlace.tlace.check_and_explain("verification/model.smv")
         return results, counterexamples
